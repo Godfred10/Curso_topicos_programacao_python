797e72fdb0a1ed9f6c48ad0cee9806639b99a2d2
GODFRED KUMI TENKORANG <1610211GTEL@COLAN96.CEFET-RJ.BR>
2018-05-11 18:25:32 -0300

    meus primeiros programas

---
+++ Aula1/exercicio 1.py
@@ -0,0 +1,8 @@
+a=input("nome")
+print("meu nome e:\n",a)
+
+b=input("altura")
+print("A minha altura e:\n",b)
+
+c=input("curso")
+print("estudo:\n",c)
---
+++ Aula1/exercicio 2.py
@@ -0,0 +1,13 @@
+x=int(input("canetas"))
+y=int(input("livros"))
+print(f"O numero de canetas é: ", [x])
+print(f"O numero de livros é: ", [y])
+x=y
+
+print(f"O número de canetas depois da troca é: ", [x])
+print(f"O número de livros depois da troca é: ", [y])
+
+
+
+
+
